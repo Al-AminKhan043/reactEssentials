feb3a73aae18bd0d6ee5857493cf705201a828f0
Al Amin <alaminkhan10009@gmail.com>
2025-01-22 02:30:46 +0600

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Header from './components/Header/Header.jsx'
 import {CORE_CONCEPTS} from './data.js'
 import TabButton from './components/tabButton.jsx';
 import { useState } from 'react';
-
+import { EXAMPLES } from './data.js';
 function App() {
-  const [selectedTopic, setSelectedTopic]=useState('Please Click a  button!');
+  const [selectedTopic, setSelectedTopic]=useState();
   
   function handleSelect(selectedfeature){
      setSelectedTopic(selectedfeature)
@@ -36,12 +36,29 @@ function App() {
         <section id="examples">
             <h2>Examples</h2>
             <menu>
-                <TabButton onSelect={()=>handleSelect('component')}>Component</TabButton>
-                <TabButton onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
-                <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
-                <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
+                <TabButton isSelected={selectedTopic==='components'} 
+                onSelect={()=>handleSelect('components')}>Component</TabButton>
+                <TabButton isSelected={selectedTopic==='jsx'} 
+                onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
+                <TabButton isSelected={selectedTopic==='props'} 
+                 onSelect={()=>handleSelect('props')}>Props</TabButton>
+                <TabButton isSelected={selectedTopic==='state'} 
+                 onSelect={()=>handleSelect('state')}>State</TabButton>
             </menu>
-              {selectedTopic}
+              
+               {!selectedTopic && <p>Please select a topic. </p> }
+                {selectedTopic? 
+                <div id='tab-content'>
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>
+                  {EXAMPLES[selectedTopic].code}
+                  </code>
+                </pre>
+              </div> : null
+               }
+               
         </section>
       </main>
     </div>
